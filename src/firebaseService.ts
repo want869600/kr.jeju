9de51cc7d8d7c263ef86be2fb4ed8926ad70a533
want869600 <want869600@gmail.com>
2026-03-09 13:20:26 +0800

    Update firebaseService.ts
    Signed-off-by: want869600 <want869600@gmail.com>

--- src/firebaseService.ts
+++ src/firebaseService.ts
@@ -99,7 +99,7 @@ export const dbService = {
   subscribeField: (field: string, cb: (data: any) => void) => {
     const ref = doc(db, 'trips', DEFAULT_TRIP_ID);
     return onSnapshot(ref, (snap) => {
-      cb(snap.exists() ? snap.data()[field] ?? {} : {});
+      cb(snap.exists() ? snap.data()[field] : undefined);
     });
   },
 
@@ -165,6 +165,7 @@ export const bookingsService = {
     await deleteDoc(docRef);
   },
 };
+
 
 
 
